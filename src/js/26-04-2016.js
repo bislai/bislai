e40041b4a1716e1a977faa41ddf5df173f1ee14a
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-01-03 21:02:34 +0100

    incluyendo todos los javascript de cada pleno

---
+++ src/js/26-04-2016.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bislai - votaciones en el Ayuntamiento de Zaragoza</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/c3.min.css">
+</head>
+
+<body>
+    <header class="font-0 border-b1 borderc-grey w-100">
+          <div class="maw-70">
+            <a href="http://bislai.co" class="w-50 textd-n  black  font-3  margin-y0  bold display-ib vertical-am font-w8">Bislai</a>
+            <div class="w-50 display-ib texta-r">
+              <a href="http://twitter.com/debislai" class="black  font-1 display-ib vertical-am textd-n texta-r padding-r1">Twitter</a>
+              <a href="http://twitter.com/debislai" class="black  font-1 display-ib vertical-am textd-n texta-r">Facebook</a>
+            </div>
+          </div>
+    </header>
+
+    <section class="maw-70 margin-y1 display-b">
+      <a class="padding-r1" href="http://www.zaragoza.es/ciudad/organizacion/plenos/detalleAnexo_AgendaInstitucional?id=651">Orden del día</a>
+      <a href="https://www.zaragoza.es/ciudad/organizacion/detalleAnexo_AgendaInstitucional?id=671">Acuerdos</a>
+
+      <div class="margin-y1">
+        <a class="btn-share  btn-share-twitter  margin-r1" href="https://twitter.com/intent/tweet?text=Bislai - Como emplean tu voto los diferentes partidos del Ayuntamiento de Zaragoza&url=http://bislai.co&via=debislai&related=debislai" rel="nofollow" target="_blank" title="Comparte en Twitter">Compartir en Twitter</a>
+                 <a class="btn-share  btn-share-facebook" href="https://facebook.com/sharer.php?u=http://bislai.co" rel="nofollow" target="_blank" title="Comparte en Facebook">Compartir en Facebook</a>
+      </div>
+    </section>
+
+    <section class="border-t1 borderc-grey w-100">
+      <article class="maw-70 padding-1-0">
+        <h3 class="border-t10 border-b10 borderc-cs padding-1-0 display-ib margin-b0">Moción 10 · Presentada por Ciudadanos</h3>
+
+        <a class="display-b margin-t1" href="http://www.zaragoza.es/ciudad/organizacion/plenos/mociones231216/cis2277.htm" target="_blank">Moción completa</a>
+
+        <p>Impulsar medidas para facilitar el relevo generacional de los autónomos y el mantenimiento de la actividad y el empleo en negocios sin sucesión empresarial.</p>
+
+        <div id="mocion10"></div>
+      </article>
+    </section>
+
+
+      <footer class="margin-y1 borderc-grey border-t1">
+        <p class="maw-70 padding-1-0">Hecho en Zaragoza con mucha maña por un <a href="http://jorgeatgu.com">maño</a> - 2017</p>
+      </footer>
+    <script type="text/javascript" src="../js/d3.min.js"></script>
+    <script type="text/javascript" src="../js/c3.min.js"></script>
+    <script type="text/javascript" src="../js/30-09-2016.js"></script>
+</body>
+</html>
