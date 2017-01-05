1514e5a29a8365b2bb9bafaf39989e9640471c06
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-01-05 21:53:30 +0100

    creando los JavaScript de cada pleno del 2015

---
+++ src/js/02-10-2015.js
@@ -0,0 +1,44 @@
+//Mocion 13
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["A favor: PP + PSOE + C'S", 19],
+           ["En contra: ZEC + CHA", 11]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159']
+    },
+     bindto: '#mocion13'
+});
+
+//Mocion 14
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["Unanimidad", 28]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159']
+    },
+     bindto: '#mocion14'
+});
+
+//Moción 14-2
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: ZEC + PSOE + C'S", 19],
+            ["En contra: CHA", 2],
+            ["Abstención: PP", 9]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159', '#a9a5a5']
+    },
+     bindto: '#mocion14-2'
+});
