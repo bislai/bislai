39a455f2ec8d513748e080a58faea652d5659540
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-01-14 22:44:46 +0100

    terminado el pleno del 27 de mayo del 2016

--- src/js/27-05-2016.js
+++ src/js/27-05-2016.js
@@ -1,19 +1,4 @@
-//Mocion 13
-var primera = c3.generate({
-    data: {
-        columns: [
-           ["A favor: PP + PSOE + C'S", 19],
-           ["En contra: ZEC + CHA", 11]
-        ],
-        type : 'pie'
-    },
-    color: {
-      pattern: ['#55AF32', '#C56159']
-    },
-     bindto: '#mocion13'
-});
-
-//Mocion 14
+//Mocion 2
 var primera = c3.generate({
     data: {
         columns: [
@@ -22,23 +7,174 @@ var primera = c3.generate({
         type : 'pie'
     },
     color: {
-      pattern: ['#55AF32', '#C56159']
+      pattern: ['#55AF32']
     },
-     bindto: '#mocion14'
+     bindto: '#mocion2'
 });
 
-//Moción 14-2
+//Mocion 3
 var primera = c3.generate({
     data: {
         columns: [
-            ["A favor: ZEC + PSOE + C'S", 19],
-            ["En contra: CHA", 2],
+           ["A favor: ZEC + PSOE + CHA", 16],
+           ["En contra: PP + C'S", 13]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159']
+    },
+     bindto: '#mocion3'
+});
+
+//Moción 4
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: ZEC + PSOE + C'S", 20],
             ["Abstención: PP", 9]
         ],
         type : 'pie'
     },
     color: {
-      pattern: ['#55AF32', '#C56159', '#a9a5a5']
+      pattern: ['#55AF32', '#a9a5a5']
     },
-     bindto: '#mocion14-2'
+     bindto: '#mocion4'
+});
+
+//Moción 5
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: ZEC + C'S + CHA", 14],
+            ["En contra: PP + PSOE", 15]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159']
+    },
+     bindto: '#mocion5'
+});
+
+//Moción 6
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: PP + PSOE + C'S", 19],
+            ["Abstención: ZEC + CHA", 10]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#a9a5a5']
+    },
+     bindto: '#mocion6'
+});
+
+//Mocion 8
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["A favor: ZEC + PSOE + C'S", 16],
+           ["En contra: PP ", 9],
+           ["Abstención: CHA", 2]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159' , '#a9a5a5']
+    },
+     bindto: '#mocion8'
+});
+
+//Mocion 9
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["A favor: PP + PSOE + C'S", 19],
+           ["En contra: ZEC ", 8],
+           ["Abstención: CHA", 2]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159' , '#a9a5a5']
+    },
+     bindto: '#mocion9'
+});
+
+//Mocion 10
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["Unanimidad", 31]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32']
+    },
+     bindto: '#mocion10'
+});
+
+//Mocion 11
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["Unanimidad", 31]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32']
+    },
+     bindto: '#mocion11'
+});
+
+
+//Mocion 12
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["A favor: PP + PSOE + C'S", 19],
+           ["En contra: ZEC ", 8],
+           ["Abstención: CHA", 2]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159' , '#a9a5a5']
+    },
+     bindto: '#mocion12'
+});
+
+//Mocion 12-2
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["Unanimidad", 31]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32']
+    },
+     bindto: '#mocion12-2'
+});
+
+//Mocion 13
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["A favor: ZEC + PSOE + C'S", 15],
+           ["En contra: PP ", 8],
+           ["Abstención: C'S", 4]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#C56159' , '#a9a5a5']
+    },
+     bindto: '#mocion13'
 });
