88a0211df8ef7982b3bf002c1a1143369ba37dc3
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-01-14 09:48:33 +0100

    terminado el pleno del 23 de marzo del 2016

--- src/js/23-03-2016.js
+++ src/js/23-03-2016.js
@@ -1,44 +1,116 @@
-//Mocion 13
+//Mocion 2
 var primera = c3.generate({
     data: {
         columns: [
-           ["A favor: PP + PSOE + C'S", 19],
-           ["En contra: ZEC + CHA", 11]
+           ["Unanimidad", 28]
         ],
         type : 'pie'
     },
     color: {
-      pattern: ['#55AF32', '#C56159']
+      pattern: ['#55AF32']
     },
-     bindto: '#mocion13'
+     bindto: '#mocion2'
 });
 
-//Mocion 14
+//Mocion 3
 var primera = c3.generate({
     data: {
         columns: [
-            ["Unanimidad", 28]
+           ["Unanimidad", 28]
         ],
         type : 'pie'
     },
     color: {
-      pattern: ['#55AF32', '#C56159']
+      pattern: ['#55AF32']
     },
-     bindto: '#mocion14'
+     bindto: '#mocion3'
 });
 
-//Moción 14-2
+//Mocion 4
 var primera = c3.generate({
     data: {
         columns: [
-            ["A favor: ZEC + PSOE + C'S", 19],
-            ["En contra: CHA", 2],
-            ["Abstención: PP", 9]
+           ["Unanimidad", 28]
         ],
         type : 'pie'
     },
     color: {
-      pattern: ['#55AF32', '#C56159', '#a9a5a5']
+      pattern: ['#55AF32']
     },
-     bindto: '#mocion14-2'
+     bindto: '#mocion4'
+});
+
+
+//Moción 5
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: PP + ZEC + C'S", 23],
+            ["Abstención: PSOE + CHA", 8]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', '#a9a5a5']
+    },
+     bindto: '#mocion5'
+});
+
+//Moción 6
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: PP + PSOE + C'S + CHA", 22],
+            ["En contra: ZEC", 9]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', "#C56159"]
+    },
+     bindto: '#mocion6'
+});
+
+//Mocion 6-2
+var primera = c3.generate({
+    data: {
+        columns: [
+           ["Unanimidad", 28]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32']
+    },
+     bindto: '#mocion6-2'
+});
+
+//Moción 8
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: ZEC + PSOE + C'S + CHA", 21],
+            ["En contra: PP", 10]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', "#C56159"]
+    },
+     bindto: '#mocion8'
+});
+
+//Moción 9
+var primera = c3.generate({
+    data: {
+        columns: [
+            ["A favor: ZEC + CHA", 11],
+            ["En contra: PP + PSOE + C'S + ", 20]
+        ],
+        type : 'pie'
+    },
+    color: {
+      pattern: ['#55AF32', "#C56159"]
+    },
+     bindto: '#mocion9'
 });
