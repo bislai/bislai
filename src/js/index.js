9a63bfe309f1e37ba3cf49dc3db0f42df2ede5be
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-01-23 23:35:14 +0100

    nuevos estilos para crear un calnedario con los plenos de cada año

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ var barra2 = document.querySelector('#zec');
 var barra3 = document.querySelector('#psoe');
 var barra4 = document.querySelector('#cs');
 var barra5 = document.querySelector('#cha');
+var barra6 = document.querySelector('#resto');
 var extremeInOutEasing = mojs.easing.path('M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0');
 squashCurve = mojs.easing.path('M0,100.004963 C0,100.004963 25,147.596355 25,100.004961 C25,70.7741867 32.2461944,85.3230873 58.484375,94.8579105 C68.9280825,98.6531013 83.2611815,99.9999999 100,100');
 
@@ -79,45 +80,16 @@ new mojs.Tween({
     }
 }).play();
 
-
-var votosFavor = [
-  {partido: 'CHA', votos: 2},
-  {partido: 'ZEC', votos: 10}
-];
-
-var votosContra = [
-  {partido: 'PP', votos: 11},
-  {partido: 'CS', votos: 4},
-  {partido: 'PSOE', votos: 6}
-
-];
-
-//Funcion para sumar votos
-function sumarVotos(arrayVotos){
-  return arrayVotos.reduce(function(acumulado, votoObj){
-    return acumulado + votoObj.votos;
-  },0);
-}
-
-//Función que obtiene el títlo de los partidos. Ejemplo: CHA + ZEC
-function tituloPartidos(arrayVotos){
-  return arrayVotos
-    .map(function(votoObj){
-      return votoObj.partido;
-    })
-    .join(' + ');
-}
-
-
-var chart = c3.generate({
-    data: {
-        columns: [
-            [tituloPartidos(votosFavor), sumarVotos(votosFavor)],
-            [tituloPartidos(votosContra), sumarVotos(votosContra)]
-        ],
-        type : 'pie'
-    },
-    color: {
-      pattern: ['#209F69', '#D01F2E']
+new mojs.Tween({
+    delay: 500,
+    duration: 1500,
+    onUpdate: function(progress) {
+        var bounceProgress = extremeInOutEasing(progress);
+        squashProgress = squashCurve(progress),
+            scaleX = 1 - 3 * squashProgress,
+            scaleY = 1 + 3 * squashProgress;
+        barra6.style.width = '10.83' * bounceProgress + '%';
+        barra6.style.transform =
+            'scaleX(' + scaleX + ') ' + 'scaleY(' + scaleY + ')';
     }
-});
+}).play();
