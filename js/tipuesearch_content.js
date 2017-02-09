1f745c7e64e761b5403ab4a50b0b64473f030264
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-09 21:42:24 +0100

    incluyendo tipusearch.js

---
+++ js/tipuesearch_content.js
@@ -0,0 +1,8 @@
+var tipuesearch = {
+    "pages": [{
+        "title": "Pleno ordinario del 30.10.2015",
+        "text": "En el sentido de que el Pleno rechace la actitud del Alcalde de Zaragoza y de la concejala de Participación Ciudadana por tratar de imponer unilateralmente y de manera autoritaria el sistema de elección de los alcaldes rurales y los vocales de las juntas vecinales (P-207/15).",
+        "tags": "Participación rurales PP",
+        "url": "http://www.tipue.com"
+    }, ]
+};
