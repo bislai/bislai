ee94687e5c3272c1192317a0ad26ee163be85d33
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-10 17:00:56 +0100

    cambiando los metodos de la busqueda de tipueSearch

--- js/tipuesearch_set.js
+++ js/tipuesearch_set.js
@@ -57,17 +57,17 @@ var tipuesearch_related = {'searches': [
 var tipuesearch_string_1 = 'No title';
 var tipuesearch_string_2 = 'Showing results for';
 var tipuesearch_string_3 = 'Search instead for';
-var tipuesearch_string_4 = '1 result';
-var tipuesearch_string_5 = 'results';
-var tipuesearch_string_6 = 'Back';
-var tipuesearch_string_7 = 'More';
-var tipuesearch_string_8 = 'Nothing found.';
+var tipuesearch_string_4 = '1 resultado';
+var tipuesearch_string_5 = 'resultados';
+var tipuesearch_string_6 = 'Atrás';
+var tipuesearch_string_7 = 'Más';
+var tipuesearch_string_8 = 'No hemos encontrado nada relacionado con tu búsqueda.';
 var tipuesearch_string_9 = 'Common words are largely ignored.';
 var tipuesearch_string_10 = 'Search too short';
 var tipuesearch_string_11 = 'Should be one character or more.';
 var tipuesearch_string_12 = 'Should be';
 var tipuesearch_string_13 = 'characters or more.';
-var tipuesearch_string_14 = 'seconds';
+var tipuesearch_string_14 = 'segundos';
 var tipuesearch_string_15 = 'Searches related to';
 
 
