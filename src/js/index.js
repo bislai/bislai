90348cd49dbe7237c2caac729be9f0fe8676228d
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-12 16:01:21 +0100

    incluyendo nuevo botón en el menu

--- src/js/index.js
+++ src/js/index.js
@@ -92,11 +92,23 @@ new mojs.Tween({
     }
 }).play();
 
-// //Javascript puro para añadir un clase con un click sin soporte para IE9
-// [].forEach.call(document.querySelector('#buscador'), function(el) {
-//     el.addEventListener('click', function() {
-//         document.querySelector('.prueba').classList.add('wrap');
-//     }, false);
-// });
+
+function appendClass(element, newClass){
+  if (element.className.length == 0) {
+    element.className = newClass;
+  }
+  else if (element.className.indexOf(newClass) < 0) {
+    element.className += ' ' + newClass;
+  }
+}
 
 
+function click(){
+  var btnBuscador = document.querySelector("#buscador");
+  var containerBuscador = document.querySelector(".containerBuscador");
+  btnBuscador.onclick = function(e){
+    appendClass(containerBuscador, "desplegado")
+  }
+}
+
+click();
