02ae1a3e3bf553d982a909305411d3db717929aa
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-12 12:59:47 +0100

    incluyendo los plenos en el buscador

--- js/tipuesearch_content.js
+++ js/tipuesearch_content.js
@@ -1,8 +1,32 @@
 var tipuesearch = {
-    "pages": [{
+    //mocion 2
+    "pages": [
+    {
         "title": "Pleno ordinario del 30.10.2015",
         "text": "En el sentido de que el Pleno rechace la actitud del Alcalde de Zaragoza y de la concejala de Participación Ciudadana por tratar de imponer unilateralmente y de manera autoritaria el sistema de elección de los alcaldes rurales y los vocales de las juntas vecinales (P-207/15).",
         "tags": "Participación rurales PP",
-        "url": "http://www.tipue.com"
-    }, ]
+        "url": "http://bislai.co/plenos/2015/30-10-2015.html"
+    },
+    //Mocion 3
+    {
+        "title": "Pleno ordinario del 30.10.2015",
+        "text": "En el sentido de que el Pleno del Ayuntamiento declare a Guillermo Toledo Monsalve (Willy Toledo), persona non grata, por su agresión gratuita a la Virgen del Pilar, patrona de nuestra ciudad (P-286/2015).",
+        "tags": "Willy PP",
+        "url": "http://bislai.co/plenos/2015/30-10-2015.html"
+    },
+    //Mocion 4
+    {
+        "title": "Pleno ordinario del 30.10.2015",
+        "text": "En el sentido de que el Pleno del Ayuntamiento inste al Gobierno de la ciudad a no incrementar las tarifas del transporte público en 2016",
+        "tags": "Autobus PP",
+        "url": "http://bislai.co/plenos/2015/30-10-2015.html"
+    },
+    //Mocion 5
+    {
+        "title": "Pleno ordinario del 30.10.2015",
+        "text": "Instando al Gobierno de Zaragoza a realizar un estudio detallado y minucioso de todos los semáforos, marcas viales y señales verticales de Zaragoza (P-331/2015).",
+        "tags": "Semaforos PP",
+        "url": "http://bislai.co/plenos/2015/30-10-2015.html"
+    },
+    ]
 };
