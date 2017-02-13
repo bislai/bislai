def7b5c6375989c3a51daf879460d76e7368a812
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-13 21:11:53 +0100

    volviendo a incluir chart

--- src/js/estadisticas.js
+++ src/js/estadisticas.js
@@ -1,4 +1,3 @@
-function animation() {
   var barra = document.querySelector('#pp');
   var barra2 = document.querySelector('#zec');
   var barra3 = document.querySelector('#psoe');
@@ -92,6 +91,40 @@ function animation() {
               'scaleX(' + scaleX + ') ' + 'scaleY(' + scaleY + ')';
       }
   }).play();
+
+
+function animation() {
+
+  var ctx = document.getElementById("myChart");
+
+  var myBarChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+  });
+
 }
 
 
@@ -105,3 +138,4 @@ new ScrollMagic.Scene({
     .setPin("#lanzando") // pins the element for the the scene's duration
     .addTo(controller) // assign the scene to the controller
     .on("enter leave", animation);
+
