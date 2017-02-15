3297bd2083e72009efc428624473a7db9b39d581
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-02-15 22:59:39 +0100

    incluyendo nuevas escenas en scrollMagic para lanzar funciones en diferentes posiciones de scroll

--- src/js/estadisticas.js
+++ src/js/estadisticas.js
@@ -1,6 +1,106 @@
 function unanimidadChart() {
 
-  var ctx = document.getElementById("myChart");
+  var ctx = document.getElementById("unanimidad-chart");
+
+  var myBarChart = new Chart(ctx, {
+      type: 'bar',
+      duration: 3000,
+      easing: 'easeInCubic',
+      options: {
+        scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true,
+                mirror:false,
+                suggestedMin: 0,
+                suggestedMax: 100,
+              }
+            }]
+        }
+      },
+      data: {
+          labels: ["unanimidad", "disconformidad"],
+          datasets: [{
+              label: 'Unanimidad',
+              data: [50 , 30],
+              backgroundColor: [
+                  'rgba(32, 159, 105, 0.2)',
+                  'rgba(255, 99, 132, 0.2)'
+                  // 'rgba(54, 162, 235, 0.2)',
+                  // 'rgba(255, 206, 86, 0.2)',
+                  // 'rgba(75, 192, 192, 0.2)',
+                  // 'rgba(153, 102, 255, 0.2)',
+                  // 'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(32, 159, 105,1)',
+                  'rgba(255,99,132,1)'
+                  // 'rgba(54, 162, 235, 1)',
+                  // 'rgba(255, 206, 86, 1)',
+                  // 'rgba(75, 192, 192, 1)',
+                  // 'rgba(153, 102, 255, 1)',
+                  // 'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+  });
+
+}
+
+function repeticionChart() {
+
+  var ctx = document.getElementById("repeticion-chart");
+
+  var myBarChart = new Chart(ctx, {
+      type: 'bar',
+      duration: 3000,
+      easing: 'easeInCubic',
+      options: {
+        scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true,
+                mirror:false,
+                suggestedMin: 0,
+                suggestedMax: 100,
+              }
+            }]
+        }
+      },
+      data: {
+          labels: ["unanimidad", "disconformidad"],
+          datasets: [{
+              label: 'Unanimidad',
+              data: [50 , 30],
+              backgroundColor: [
+                  'rgba(32, 159, 105, 0.2)',
+                  'rgba(255, 99, 132, 0.2)'
+                  // 'rgba(54, 162, 235, 0.2)',
+                  // 'rgba(255, 206, 86, 0.2)',
+                  // 'rgba(75, 192, 192, 0.2)',
+                  // 'rgba(153, 102, 255, 0.2)',
+                  // 'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(32, 159, 105,1)',
+                  'rgba(255,99,132,1)'
+                  // 'rgba(54, 162, 235, 1)',
+                  // 'rgba(255, 206, 86, 1)',
+                  // 'rgba(75, 192, 192, 1)',
+                  // 'rgba(153, 102, 255, 1)',
+                  // 'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      }
+  });
+
+}
+
+function masMocionesChart() {
+
+  var ctx = document.getElementById("mas-mociones-chart");
 
   var myBarChart = new Chart(ctx, {
       type: 'bar',
@@ -51,12 +151,36 @@ function unanimidadChart() {
 
 var controller = new ScrollMagic.Controller();
 
-// create a scene
 new ScrollMagic.Scene({
-        duration: 100,    // the scene should last for a scroll distance of 100px
-        offset: 90        // start this scene after scrolling for 50px
+        duration: 200,
+        offset: 300
     })
-    .setPin("#unanimidad") // pins the element for the the scene's duration
-    .addTo(controller) // assign the scene to the controller
+    .setPin("#unanimidad")
+    .addTo(controller)
     .on("enter", unanimidadChart);
+
+var repeticion = document.getElementById('repeticion');
+var repeDimension = repeticion.getBoundingClientRect();
+var repeTop = repeDimension.top - 50;
+
+new ScrollMagic.Scene({
+        duration: 200,
+        offset: repeTop
+    })
+    .setPin("#repeticion")
+    .addTo(controller)
+    .on("enter", repeticionChart);
+
+var masMociones = document.getElementById('mas-mociones');
+var masMocionesDimension = masMociones.getBoundingClientRect();
+var masMocionesTop = masMocionesDimension.top - 50;
+
+new ScrollMagic.Scene({
+        duration: 200,
+        offset: masMocionesTop
+    })
+    .setPin("#mas-mociones")
+    .addTo(controller)
+    .on("enter", masMocionesChart);
+
 
