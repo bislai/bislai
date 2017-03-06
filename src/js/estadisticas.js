d9e4fc9675991361d3593cda66f9fadd8fb6038a
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-03-06 22:01:33 +0100

    eliminando javascript que sobra y ajustando estadisticas en el responsive

--- src/js/estadisticas.js
+++ src/js/estadisticas.js
@@ -134,115 +134,6 @@ function masMocionesChart() {
 
 }
 
-function perdidoChart() {
-
-  var ctx = document.getElementById("perdido-mociones-chart");
-
-  var myBarChart = new Chart(ctx, {
-      type: 'bar',
-      duration: 3000,
-      easing: 'easeInCubic',
-      options: {
-        scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero:true,
-                mirror:false,
-                suggestedMin: 0,
-                suggestedMax: 100,
-              }
-            }]
-        },
-        legend: {
-            display: false
-        }
-      },
-      data: {
-          labels: ["unanimidad", "disconformidad"],
-          datasets: [{
-              label: 'Unanimidad',
-              data: [50 , 30],
-              backgroundColor: [
-                  'rgba(32, 159, 105, 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
-                  // 'rgba(54, 162, 235, 0.2)',
-                  // 'rgba(255, 206, 86, 0.2)',
-                  // 'rgba(75, 192, 192, 0.2)',
-                  // 'rgba(153, 102, 255, 0.2)',
-                  // 'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(32, 159, 105,1)',
-                  'rgba(255,99,132,1)'
-                  // 'rgba(54, 162, 235, 1)',
-                  // 'rgba(255, 206, 86, 1)',
-                  // 'rgba(75, 192, 192, 1)',
-                  // 'rgba(153, 102, 255, 1)',
-                  // 'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      }
-  });
-
-}
-
-function ganadoChart() {
-
-  var ctx = document.getElementById("ganado-mociones-chart");
-
-  var myBarChart = new Chart(ctx, {
-      type: 'bar',
-      duration: 3000,
-      easing: 'easeInCubic',
-      options: {
-        scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero:true,
-                mirror:false,
-                suggestedMin: 0,
-                suggestedMax: 100,
-              }
-            }]
-        },
-        legend: {
-            display: false
-        }
-      },
-      data: {
-          labels: ["unanimidad", "disconformidad"],
-          datasets: [{
-              label: 'Unanimidad',
-              data: [50 , 30],
-              backgroundColor: [
-                  'rgba(32, 159, 105, 0.2)',
-                  'rgba(255, 99, 132, 0.2)'
-                  // 'rgba(54, 162, 235, 0.2)',
-                  // 'rgba(255, 206, 86, 0.2)',
-                  // 'rgba(75, 192, 192, 0.2)',
-                  // 'rgba(153, 102, 255, 0.2)',
-                  // 'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(32, 159, 105,1)',
-                  'rgba(255,99,132,1)'
-                  // 'rgba(54, 162, 235, 1)',
-                  // 'rgba(255, 206, 86, 1)',
-                  // 'rgba(75, 192, 192, 1)',
-                  // 'rgba(153, 102, 255, 1)',
-                  // 'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      legend: {
-          display: false
-      }
-  });
-
-}
-
 function abstenidoChart() {
 
   var ctx = document.getElementById("abstencion-mociones-chart");
@@ -429,31 +320,6 @@ if(width > 1024) {
       .on("enter", masMocionesChart);
 
 
-  var perdidoMociones = document.getElementById('perdido-mociones');
-  var perdidoMocionesDimension = perdidoMociones.getBoundingClientRect();
-  var perdidoMocionesTop = perdidoMocionesDimension.top - 50;
-
-  new ScrollMagic.Scene({
-          duration: 200,
-          offset: perdidoMocionesTop
-      })
-      .setPin("#perdido-mociones")
-      .addTo(controller)
-      .on("enter", perdidoChart);
-
-
-  var ganadoMociones = document.getElementById('ganado-mociones');
-  var ganadoMocionesDimension = ganadoMociones.getBoundingClientRect();
-  var ganadoMocionesTop = ganadoMocionesDimension.top - 50;
-
-  new ScrollMagic.Scene({
-          duration: 200,
-          offset: ganadoMocionesTop
-      })
-      .setPin("#ganado-mociones")
-      .addTo(controller)
-      .on("enter", ganadoChart);
-
 
   var abstenidoMociones = document.getElementById('abstenido-mociones');
   var abstenidoMocionesDimension = abstenidoMociones.getBoundingClientRect();
@@ -498,8 +364,6 @@ if(width > 1024) {
     unanimidadChart();
     repeticionChart();
     masMocionesChart();
-    perdidoChart();
-    ganadoChart();
     abstenidoChart();
     encontraChart();
     afavorChart();
