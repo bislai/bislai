33e1acac52563f546181a12870218e93b3dd3e24
Jorge Aznar <jorgeatgu@gmail.com>
2018-03-25 21:26:43 +0200

    incluyendo correciones en los textos de las estadisticias de cada partido

--- src/js/mapa-cs.js
+++ src/js/mapa-cs.js
@@ -1,3 +1,7 @@
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+
+
 $(function() {
     d3.json("mapas/distritos-ciudadanos-zaragoza.geojson", function(err, data) {
         mapDraw(data);
@@ -64,26 +68,3 @@ $(function() {
         }
     }
 });
-
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'mociones.json', true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);
- }
-
- function init() {
-  loadJSON(function(response) {
-   // Parse JSON string into object
-     var actual_JSON = JSON.parse(response);
-     console.log(actual_JSON[0].fecha);
-  });
- }
