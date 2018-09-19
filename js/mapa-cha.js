d7cc2027fc1b2b2b40ace7f0590e324c2bf1a4d7
Jorge Aznar Tobajas <jorgeatgu@jorgeatgu.com>
2018-09-19 19:43:07 +0200

    nueva grafica

--- js/mapa-cha.js
+++ js/mapa-cha.js
@@ -314,3 +314,289 @@ if (width > 769) {
 } else {
     initResponsive();
 }
+
+// function multiple() {
+
+//     var margin = { top: 24, right: 24, bottom: 24, left: 24 },
+//         width = 600 - margin.left - margin.right,
+//         height = 400 - margin.top - margin.bottom;
+
+//     var svg = d3.select('.grafica-favor-legislatura-graph')
+//         .attr('class', 'grafica-favor-legislatura-chart')
+//         .append("g")
+//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//     var parseDate = d3.timeParse("%x");
+
+//     var x = d3.scaleTime().range([0, width]);
+//     var y = d3.scaleLinear().range([height, 0]);
+
+//     var bisectDate = d3.bisector(function(d) { return d.fecha; }).left;
+
+//     var yAxis = d3.axisLeft(y)
+//         .tickSize(-width)
+//         .tickFormat(d3.format("d"))
+//         .ticks(4);
+
+//     var priceline = d3.line()
+//         .x(function(d) { return x(d.fecha); })
+//         .y(function(d) { return y(d.votos); })
+
+
+//     d3.csv("datos/cha/legislatura-cha-votos-a-favor.csv", function(error, data) {
+
+//         data.forEach(function(d) {
+//             d.fecha = parseDate(d.fecha);
+//             d.votos = d.votos;
+//         });
+
+//         x.domain(d3.extent(data, function(d) { return d.fecha; }));
+//         y.domain([0, 8]);
+
+//         var dataComb = d3.nest()
+//             .key(function(d) { return d.presentada; })
+//             .entries(data);
+
+
+//         var colors = ["#9a1622", "#e30613", "#0080b8", "#f07a36"]
+
+//         var color = d3.scaleOrdinal(colors);
+
+//         dataComb.forEach(function(d) {
+//             svg.append("path")
+//                 .attr("class", "line " + d.key)
+//                 .style("stroke", function() {
+//                     return d.color = color(d.key)
+//                 })
+//                 .attr("d", priceline(d.values));
+
+//         });
+
+//         svg.append("g")
+//             .attr("class", "xAxis")
+//             .attr("transform", "translate(0," + height + ")")
+//             .call(d3.axisBottom(x));
+
+//         svg.append("g")
+//             .attr("class", "yAxis")
+//             .call(yAxis);
+
+//     });
+// }
+
+// multiple()
+
+function barscatter() {
+    //Estructura similar a la que utilizan en algunos proyectos de pudding.cool
+    var margin = { top: 24, right: 24, bottom: 24, left: 24 };
+    var width = 0;
+    var height = 0;
+    var w = 0;
+    var h = 0;
+    var chart = d3.select('.grafica-favor-legislatura');
+    var svg = chart.select('svg');
+    var scales = {};
+    var dataz = void 0;
+    var colors = ["#9a1622", "#e30613", "#0080b8", "#f07a36"];
+    var color = d3.scaleOrdinal(colors);
+    var parseDate = d3.timeParse("%x");
+
+    //Escala para los ejes X e Y
+    function setupScales() {
+
+        var countX = d3.scaleLinear().domain([d3.min(dataz, function (d) {
+            return d.fecha;
+        }), d3.max(dataz, function (d) {
+            return d.fecha;
+        })]);
+
+        var countY = d3.scaleLinear().domain([d3.min(dataz, function (d) {
+            return d.votos;
+        }), d3.max(dataz, function (d) {
+            return d.votos;
+        })]);
+
+        scales.count = { x: countX, y: countY };
+    }
+
+    //Seleccionamos el contenedor donde irán las escalas y en este caso el area donde se pirntara nuestra gráfica
+    function setupElements() {
+
+        var g = svg.select('.grafica-favor-legislatura-container');
+
+        g.append('g').attr('class', 'axis axis-x');
+
+        g.append('g').attr('class', 'axis axis-y');
+
+        g.append('g').attr('class', 'area-container-chart-scatter');
+    }
+
+    //Actualizando escalas
+    function updateScales(width, height) {
+        scales.count.x.range([0, width]);
+        scales.count.y.range([height, 20]);
+    }
+
+    //Dibujando ejes
+    function drawAxes(g) {
+
+        var axisX = d3.axisBottom(scales.count.x).tickFormat(d3.timeFormat("%Y")).tickSize(-height).ticks(3);
+
+        g.select(".axis-x").attr("transform", "translate(0," + height + ")").call(axisX);
+
+        var axisY = d3.axisLeft(scales.count.y).tickFormat(function (d) {
+            return d;
+        }).tickSize(-width).ticks(5);
+
+        g.select(".axis-y").call(axisY);
+    }
+
+    function updateChart(dataz) {
+        w = chart.node().offsetWidth;
+        h = 600;
+
+        width = w - margin.left - margin.right;
+        height = h - margin.top - margin.bottom;
+
+        svg.attr('width', w).attr('height', h);
+
+        var translate = "translate(" + margin.left + "," + margin.top + ")";
+
+        var g = svg.select('.grafica-favor-legislatura-container');
+
+        g.attr("transform", translate);
+
+        updateScales(width, height);
+
+        var container = chart.select('.area-container-chart-scatter');
+
+        var layer = container.selectAll('.scatter-circles').data(dataz);
+
+        var newLayer = layer.enter().append('image').attr('class', 'scatter-circles');
+
+        // layer.merge(newLayer)
+        //     .attr("x", function(d) {
+        //         return scales.count.x(d.fecha);
+        //     })
+        //     .attr("y", function(d) {
+        //         return scales.count.y(d.votos);
+        //     })
+        //     .attr('class', function(d) {
+        //         return d.presentada;
+        //     })
+        //     .attr("r", 0)
+        //     .transition()
+        //     .ease(d3.easeSin)
+        //     .duration(1200)
+        //     .attr("r", function(d) {
+        //         if (d.votos >= 1) {
+        //             return d.votos * 7
+        //         } else  {
+        //             return 2;
+        //         }
+        //     })
+        //     .style("fill", function(d) {
+        //         return d.color = color(d.presentada)
+        //     })
+        //     .style("stroke", function(d) {
+        //         return d.color = color(d.presentada)
+        //     })
+        //     .style("opacity", 0.7);
+
+        //Las risas!
+        layer.merge(newLayer).attr("x", function (d) {
+            return scales.count.x(d.fecha) - 5;
+        }).attr("y", function (d) {
+            return scales.count.y(d.votos) - 10;
+        }).attr('class', function (d) {
+            return d.presentada;
+        });
+
+        container.selectAll('.PSOE').attr("xlink:href", 'img/psoe.svg').attr("width", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        }).attr("height", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        });
+
+        container.selectAll('.CIUDADANOS').attr("xlink:href", 'img/ciudadanos.svg').attr("width", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        }).attr("height", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        });
+
+        container.selectAll('.ZEC').attr("xlink:href", 'img/zec.svg').attr("width", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        }).attr("height", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        });
+
+        container.selectAll('.PP').attr("xlink:href", 'img/pp.svg').attr("width", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        }).attr("height", function (d) {
+            if (d.votos >= 1) {
+                return d.votos * 8;
+            } else {
+                return 16;
+            }
+        });
+
+        drawAxes(g);
+    }
+
+    function resize() {
+        updateChart(dataz);
+    }
+
+    // LOAD THE DATA
+    function loadData() {
+
+        d3.csv('datos/cha/legislatura-cha-votos-a-favor.csv', function (error, data) {
+            if (error) {
+                console.log(error);
+            } else {
+                dataz = data;
+                dataz.forEach(function (d) {
+                    d.fecha = parseDate(d.fecha);
+                    d.votos = +d.votos;
+                });
+                setupElements();
+                setupScales();
+                updateChart(dataz);
+            }
+        });
+    }
+
+    window.addEventListener('resize', resize);
+
+    loadData();
+}
+
+barscatter();
